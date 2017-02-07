1198e26b8382035f4b06fc7407d218acfd8f5f0b
James Richford <jamesrichford@googlemail.com>
2017-02-07 08:36:21 +0000

    simple auth example

--- server/services/home.service.ts
+++ server/services/home.service.ts
@@ -1,11 +1,41 @@
 import { Controller, Get } from 'inversify-express-utils';
 import { injectable } from 'inversify';
+import { Request, Response } from "express";
+
+export enum UserRole {
+  This,
+  That,
+  TheOther
+}
+
+export function Authorize(...roles: Array<UserRole>) {
+    return (target: any,
+                           decoratedPropertyKey: string,
+                           descriptor: TypedPropertyDescriptor<(request: Request, response: Response) => any>) => {
+    const originalService = descriptor.value;
+
+    descriptor.value = (request: Request, response: Response) => {
+      console.log("authorizing...");
+
+      if (new Date().getTime() % 2) {
+        response.status(401);
+
+        return "get out of here";
+      }
+
+      return originalService.call(target, request, response);
+    };
+};
+}
 
 @injectable()
 @Controller('/')
 export class HomeService {
+
   @Get('/')
+  @Authorize(UserRole.This)
   public get(): string {
+    console.log(this);
     return 'Home sweet home';
   }
 }
