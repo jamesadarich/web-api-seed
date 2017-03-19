4b536cfe42bb45e347623e5968811296886af87a
James Richford <jamesrichford@googlemail.com>
2017-03-19 20:54:17 +0000

    install nodemailer

--- server/repositories/user.repository.ts
+++ server/repositories/user.repository.ts
@@ -12,11 +12,13 @@ export class UserRepository {
         this._setupRepo();
     }
 
-    private async _setupRepo() {
+    private _setupRepo() {
 
         const sqlConfig = config.databases.sql;
 
-        const connection = await createConnection({
+        console.log("connecting...");
+    
+        createConnection({
             driver: {
                 type: "mssql",
                 host: sqlConfig.host,
@@ -26,17 +28,24 @@ export class UserRepository {
                 database: sqlConfig.databaseName
             },
             entities: [
-                path.join(__dirname, "../models/**/*.modeljs")
+                path.join(__dirname, "../models/user.model.js")
             ],
             autoSchemaSync: true
-        });
-
-        this._ormRepository = connection.getRepository(UserModel);
+        })
+        .then((connection) => {
+            console.log("connected", connection);
+            this._ormRepository = connection.getRepository(UserModel);
+            console.log("repo", this._ormRepository);
+        })
+        .catch((e) => console.log("did not connect", e));
     }
 
     private _ormRepository: Repository<UserModel>;
 
     public async getAllUsers() {
+        while(!this._ormRepository) {
+
+        }
         return await this._ormRepository.find(UserModel);
     }
 
